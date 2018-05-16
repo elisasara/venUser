d1b5447b8031c6a172e1712770753f0fa3a4092c
Elisa Urda <urda.elisa@gmail.com>
2018-05-15 20:09:42 -0400

    added JAWSDB variable

--- config/config.js
+++ config/config.js
@@ -17,11 +17,13 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": process.env.MySQL_Database_Password,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
+    // "username": "root",
+    // "password": process.env.MySQL_Database_Password,
+    // "database": "database_production",
+    // "host": "127.0.0.1",
+    // "dialect": "mysql"
   }
 };
 
