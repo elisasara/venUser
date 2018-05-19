1118e9c273ff660c79fbfd60e400fbdc6a770310
Elisa Urda <urda.elisa@gmail.com>
2018-05-19 11:13:56 -0400

    working on second foursquare call and route to correct template

--- public/js/venueResult.js
+++ public/js/venueResult.js
@@ -2,24 +2,19 @@ var request = require("request");
 
 // get the inputs from each of the fields
 
-// module.exports = function (id, searchResults) {
+module.exports = function (VENUE_ID, callback) {
+request({
+    url: "https://api.foursquare.com/v2/venues/" + VENUE_ID,
+    method: "GET",
+    qs: {
+        client_id: "DJI2IAR1JVBJHOKACMOGQ0D31UFJJUGEGYOSQ3L30OSYWVUP",
+        client_secret: "LD2PAKPD1Y5PGJOPWMWWJI2X5Z5NFTQ1JUSYBL0AFFATQ50D",
+        v: 20180515
+    },
+}, function(err, res, body){
+    console.log(JSON.parse(body));
+});
+}
 
-// }
 
 
-
-
-// this gives you venue url, social media info, hours
-// var VENUE_ID = "55dda7ed498e67592140ff21";
-
-// request({
-//     url: "https://api.foursquare.com/v2/venues/" + VENUE_ID,
-//     method: "GET",
-//     qs: {
-//         client_id: "DJI2IAR1JVBJHOKACMOGQ0D31UFJJUGEGYOSQ3L30OSYWVUP",
-//         client_secret: "LD2PAKPD1Y5PGJOPWMWWJI2X5Z5NFTQ1JUSYBL0AFFATQ50D",
-//         v: 20180515
-//     },
-// }, function(err, res, body){
-//     console.log(JSON.parse(body));
-// })
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,19 +1,16 @@
 var express = require("express");
-
-// var router = express.Router();
 var app = express();
-
 var review = require("../models/post.js")
-
-var venue = require("../public/js/venueSearch.js");
+var venueSearch = require("../public/js/venueSearch.js");
+var venueResult = require("../public/js/venueResult.js");
 
 
 
 module.exports = function (app) {
 
 
-    app.get('/', function (req, res) {
-        res.render('index');
+    app.get("/", function (req, res) {
+        res.render("index");
     });
 
     // app.get('/search', function(req,res) {
@@ -22,32 +19,51 @@ module.exports = function (app) {
 
     app.get("/venues", function (req, res) {
         // run the function passed in through venueSearch
-        venue(function (err, result) {
+        venueSearch(function (err, result) {
             // render the results of the foursquare API call to the results handlebars file
             res.render("results", { venue: result });
         })
     });
 
     app.get("/venues/:id", function (req, res) {
-        review.findAll(function (data) {
-            { where: venue_id = req.params.id }
-            var reviewObj = {
-                reviews: data
-            };
-            console.log(reviewObj);
-            res.render("venue", reviewObj);
+        // get id of venue to pass into foursquare call
+        var venueId = req.params.id;
+
+        // run foursquare call and return info as an object
+        venueResult(function (venueId, err, result) {
+            // get all of the info from the database
+            review.findAll(function (data) {
+                { where: venue_id = req.params.id }
+                // create one object with info from foursquare and from db
+                var venueObj = {
+                    venueInfo: result,
+                    reviews: data
+                };
+                console.log(venueObj);
+                // render to correct handlebars template
+                res.render("select", venueObj);
+            });
         });
+
+        // review.findAll(function (data) {
+        //     { where: venue_id = req.params.id }
+        //     var reviewObj = {
+        //         reviews: data
+        //     };
+        //     console.log(reviewObj);
+        //     res.render("venue", reviewObj);
+        // });
     });
 
-    app.post("/api/venues", function (req, res) {
-        review.create([
-            // check these names with database
-            "name_author", "category", "content"
-        ], [
-                req.body.name, req.body.type, req.body.review
-            ], function (result) {
-                res.json({ id: result.insertId });
-            });
-    });
+    // app.post("/api/venues", function (req, res) {
+    //     review.create([
+    //         // check these names with database
+    //         "name_author", "category", "content"
+    //     ], [
+    //             req.body.name, req.body.type, req.body.review
+    //         ], function (result) {
+    //             res.json({ id: result.insertId });
+    //         });
+    // });
 
 }
