06ea8f62a6c381b9c295ba531dd5fed6fd42d458
Elisa Urda <urda.elisa@gmail.com>
2018-05-19 10:56:46 -0400

    updated first foursquare API call

--- public/js/venueSearch.js
+++ public/js/venueSearch.js
@@ -1,44 +1,51 @@
+require("dotenv").config();
+
 var request = require("request");
-
-// get the inputs from each of the fields
-
 
 
 module.exports = function (callback) {
+    // on click event for search submit button
+    $("searchSubmit").on("click", function (event) {
+        event.preventDefault();
+        // get the inputs from each of the fields
+        var city = $("#city").val().trim();
+        var state = $("state").val().trim();
+        var venueName = $("venue").val().trim();
 
-    request({
-        url: "https://api.foursquare.com/v2/venues/search",
-        method: "GET",
-        qs: {
-            client_id: "DJI2IAR1JVBJHOKACMOGQ0D31UFJJUGEGYOSQ3L30OSYWVUP",
-            client_secret: "LD2PAKPD1Y5PGJOPWMWWJI2X5Z5NFTQ1JUSYBL0AFFATQ50D",
-            near: "Philadelphia, PA",
-            query: "Fillmore",
-            v: 20180515,
-            limit: 10
-        },
-    }, function (err, res, body) {
-        if (err) {
-            callback(err);
-        }
-        var venues = JSON.parse(body);
-        var venueInfo = venues.response.venues;
-        var searchResults = [];
-        for (var i=0; i<venueInfo.length; i++) {
-            // create object for diplaying info in handlebars
-            var eachResult = {
-                id: venueInfo[i].id,
-                name: venueInfo[i].name,
-                address: venueInfo[i].location.address,
-                city: venueInfo[i].location.city,
-                state: venueInfo[i].location.state,
-                zip: venueInfo[i].location.postalCode
+        request({
+            url: "https://api.foursquare.com/v2/venues/search",
+            method: "GET",
+            qs: {
+                client_id: process.env.fourSquare_API_client_id,
+                client_secret: process.env.fourSquare_API_client_secret,
+                near: city + ", " + state,
+                query: venueName,
+                v: 20180515,
+                limit: 10
+            },
+        }, function (err, res, body) {
+            if (err) {
+                callback(err);
             }
-            // push each object into the array
-            searchResults.push(eachResult);
-        }
+            var venues = JSON.parse(body);
+            var venueInfo = venues.response.venues;
+            var searchResults = [];
+            for (var i = 0; i < venueInfo.length; i++) {
+                // create object for diplaying info in handlebars
+                var eachResult = {
+                    id: venueInfo[i].id,
+                    name: venueInfo[i].name,
+                    address: venueInfo[i].location.address,
+                    city: venueInfo[i].location.city,
+                    state: venueInfo[i].location.state,
+                    zip: venueInfo[i].location.postalCode
+                }
+                // push each object into the array
+                searchResults.push(eachResult);
+            }
 
-        callback(null, searchResults);
+            callback(null, searchResults);
+        });
     });
 };
 
