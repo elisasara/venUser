84f016bb91567b9654efd1c1e597386416137210
MichaelBorrasso <michaelborrasso@gmail.com>
2018-05-26 08:21:16 -0400

    5-26-18 push

---
+++ tests/nightmareTest.js
@@ -0,0 +1,24 @@
+var Nightmare = require("nightmare");
+var expect = require("chai").expect;
+
+describe("venUser", function() {
+  // The default tests in mocha is 2 seconds.
+  // Extending it to 30 seconds to have time to load the pages
+
+  this.timeout(30000);
+  it("should send user to main page", function(done) {
+    // ID for the login button.
+    Nightmare({ show: true })
+      .goto("https://obscure-chamber-89134.herokuapp.com/venues")
+      // Click the catalog link
+      .click("#searchSubmit")
+      // Evaluate the title
+      .evaluate(function() {
+        return document.title;
+      })
+      // is the title as expected?
+      .then(function(title) {
+        expect(title).to.equal("venUser ");
+        done();
+      });
+  })})
