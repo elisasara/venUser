cfcc23ee4c7e842cfa028f5c717809b6d3c117e7
Elisa Urda <urda.elisa@gmail.com>
2018-05-30 18:25:36 -0400

    a few small fixes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -149,7 +149,6 @@ module.exports = function (app) {
         app.post("/api/reviews", function (req, res) {
             db.Review.create(req.body).then(function (data) {
                 res.json(data);
-
             });
         });
 
